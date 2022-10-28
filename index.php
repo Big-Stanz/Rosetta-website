5d02d7da2b896c4cf28c3d415782f9ed2def2eb8
Big-Stanz <98669539+Big-Stanz@users.noreply.github.com>
2022-10-28 09:32:18 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>
+        rosetta 
+      </title>
+      <meta name="viewport" content="width=device-width initial-scale=1.0">
+      <link rel="stylesheet" href="./styles.css">
+    </head>
+
+    
+    <body>
+        <header>
+          <h3><b>Rosetta</b></h3>
+
+          <ul class="nav">
+            <li class="nav-link"><a href="#home">Home</a></li>
+            <li class="nav-link"><a href="#about">About</a></li>
+            <li class="nav-link"><a href="#services">Services</a></li>
+            <li class="nav-link"><a href="#contact">Contact us</a></li>
+          </ul>
+        </header>
+
+        <div id="home" class="intro horizontally-centered">
+          <h1 class="text-centered">ROSETTA</h1>
+          <p class="text-centered">Exploring infinite possibilities through technology</p>
+          <img class="horizontally-centered" src="./img/intro.webp" alt="into img">
+        </div>
+
+        <div id="about" class="horizontally-centered">
+          <h1 class="heading text-centered">ABOUT</h1>
+          <div class="flex">
+            <p class="description">
+              Rosetta Technologies Inc is a modern state of the art tech company 
+              formed in the spring of 2022 that is focused on providing different
+              services in the tech field.
+            </p>
+            <img src="./img/hdDarkCube.jpeg" >
+          </div>
+        </div>
+
+        <div id="services" class="horizontally-centered">
+          <h1 class="heading text-centered">SERVICES</h1>
+          
+          <div class="flex" id="displayBlock-onSmallScreen">
+            <div class="flex" id="displayBlock-onMediumScreen">
+              <div class="service">
+                <img src="./img/consulting.jpeg" class="horizontally-centered">
+                <h3 class="sub-heading text-centered">CONSULTING</h3>
+                <p>Rosetta is home to technology experts who boast a considerable amount 
+                  of years of experience in the industry and re ready to share their 
+                  knowledge and experience about the same
+                </p>
+              </div>
+
+              <div class="service">
+                <img src="./img/VoIP.jpeg" class="horizontally-centered">
+                <h3 class="sub-heading text-centered">VoIP</h3>
+                <p>Rosetta provides next level calls over the internet with the 
+                  best voice quality  and unbeatable prices in the market.
+                </p>
+              </div>
+            </div>
+
+            <div  class="service">
+              <img src="./img/codeOnScreen.jpeg" class="horizontally-centered">
+              <h3 class="sub-heading text-centered">Web Hosting</h3>
+              <p>Rosetta offers web hosting services at the  best rates ever .
+              </p>
+            </div>
+          </div>
+
+          
+        </div>
+
+        <div id="contact" class="horizontally-centered">
+          <h1 class="heading text-centered">CONTACT US</h1>
+          <p class="text-centered">Nairobi,Nairobi Province, kenya</p>
+          <a href="mailto:stanlypiuomondi@gmail.com" class="">
+            
+              <i class="fa fa-envelope"></i>stanlypiuomondi@gmail.com
+            
+          </a>
+
+          <input type="text" placeholder="Name" class="horizontally-centered">
+          <input type="text" placeholder="Phone" class="horizontally-centered">
+          <input type="text" placeholder="Email address" class="horizontally-centered">
+          <textarea class="horizontally-centered" placeholder="Message" columns="5"></textarea>
+
+          <button class="horizontally-centered"><h3>CONTACT US</h3></button>
+        </div>
+
+        <div class="location">
+
+        </div>
+
+        <footer>
+          <div>
+            <h5>rosetta</h5>
+            <p>Copyright &copy; 2022 All rights reserved</p>
+          </div>
+
+          <div>
+            <ul class="nav">
+            <li class="nav-link"><a href="#home">HOME</a></li>
+            <li class="nav-link"><a href="#about">ABOUT</a></li>
+            <li class="nav-link"><a href="#services">SERVICES</a></li>
+            <li class="nav-link"><a href="#contact">CONTACT US</a></li>
+            </ul>
+          </div>
+        </footer>
+    </body>
+</html>
+
+
+
